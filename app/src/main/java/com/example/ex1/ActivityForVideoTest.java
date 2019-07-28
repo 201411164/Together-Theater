ed8faf871254a8eb915cdb4927b19dad7d843944
g5yoon <g5yoon1214@naver.com>
2019-07-28 18:09:28 +0900

    영상 배치를 위한 알고리즘 설계
    테스트용 변수값 지정
    
    가상 디바이스 에뮬레이터 실행이 되지 않아 추후에 다시 테스트 할 예정
    
    이해하기 편하게 주석 자세하게 달았음

--- app/src/main/java/com/example/ex1/ActivityForVideoTest.java
+++ app/src/main/java/com/example/ex1/ActivityForVideoTest.java
@@ -1,64 +1,78 @@
 package com.example.ex1;
 
+import android.view.ViewGroup;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.VideoView;
 import android.widget.MediaController;
 import android.net.Uri;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 
 
 public class ActivityForVideoTest extends AppCompatActivity {
-    public static int user = 1;//사용자 식별번호
-    int aW,bW,cW,aH,bH,cH = 0;//화면 분할을 위한 각 디바이스 가로세로 길이
-    int sH;//기기 abc 중 가장 작은 높이값
-    int W,H=0;//결정된 비디오뷰의 길이
-    int aX,bX,cX = 0;//좌표 이동을 위한 각 기기의 X값
+    //컨스트레인트 레이아웃 정의
+    ConstraintSet mConstraintSet = new ConstraintSet();
+    ConstraintLayout mConstraintLayout;
 
+    int user = 1;//사용자 식별번호, 호스트 기기에만 미디어컨트롤러가 나오도록 하기 위함(user 변수의 값이 1인 경우에만 나오게 함)
+    int aW,bW,cW,aH,bH,cH = 0;//화면 분할을 위한 각 디바이스 가로세로 길이
+    int sH=0;//기기 a b c 중 가장 작은 높이값
+    int W,H=0;//결정된 레이아웃의 길이
+    int aX,bX,cX = 0;//좌표 이동을 위한 각 기기의 X값
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_for_video_test);                                               //비디오뷰 생성을 위한 부분
+        //컨스트레인트 레이아웃 내부에 비디오뷰 생성 비디오뷰 사이즈는 레이아웃과 동일, 이후 비디오뷰 위치 조정은 setX 매소드를 이용
+        setContentView(R.layout.activity_for_video_test);
+        mConstraintLayout = (ConstraintLayout) findViewById(R.id.cL1);
+        mConstraintSet.clone(mConstraintLayout);
         VideoView vv =(VideoView)findViewById(R.id.videoView1);
         Uri video = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.test2);
         vv.setVideoURI(video);
 
-        W=aW+bW+cW;
-        H=(aW+bW+cW)/16*9;                                                                               //영상 배치를 위한 부분
-        W=1920;//테스트용 임시값
-        H=1080;//테스트용 임시값
-        sH=1060;//테스트용 임시값
-        if(H>sH) {
-            H = sH;
-            W = sH / 9 * 16;
-            W=1920;
-            H=1080;
-            vv.setLayoutParams(new FrameLayout.LayoutParams(W, H));
-            LinearLayout linearLayout = (LinearLayout) findViewById(R.id.info);
-            linearLayout.addView(vv);
 
-            vv.setX(aX);
-        }
+
+        //비디오뷰의 높이 설정을 위한 디바이스들 중 높이 최소값 구해 sH에 저장, 함수화 필요
+        if(aH<bH){
+            if(aH<cH){
+                sH=aH;}
+            else{
+                sH=cH; } }
         else{
-            return;
-        }
+            if(cH<bH){
+                sH=cH; }
+            else{
+                sH=bH; } }
 
-        if(user==1) {                                                                                       //호스트 기기에만 미디어 컨트롤러가 나오도록 하는 부분
-            final MediaController mc = new MediaController(this);
-            vv.setMediaController(mc);
-        }
-        else {
-            return;
-        }
-//앞으로 필요한 기능 - 별도의 변수 추가하여 각 기기마다 독립적 view 제공
+        //결정된 레이아웃의 높이, 넓이값 정의
+        W=aW+bW+cW;
+        H=sH;
+
+
+        //레이아웃 사이즈 조정(layoutparams 매소드 이용)
+        setContentView(R.layout.activity_for_video_test);
+        ConstraintLayout.LayoutParams mLayoutParams = (ConstraintLayout.LayoutParams)vv.getLayoutParams();
+        mLayoutParams.height=1920;
+        mLayoutParams.width=1080;
+        mConstraintLayout.setLayoutParams(mLayoutParams);
+
+
+        //미디어 컨트롤러 생성
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(vv);
+        mediaController.setPadding(0,0,0,0);
+        vv.setMediaController(mediaController);
+        vv.start();
+
+//앞으로 필요한 기능 - 별도의 변수(int user) 추가하여 각 기기마다 독립적 view 제공
 //기기 배치를 위한 가이드라인 제공
 //영상 일시정지 시 정지한 시간 다른 기기로 전송, 그 시간에 맞추어 정지
 // 다시 재생 시 정해진 시간(위성시계 기준)에 동시 재생 시작
 //화면 배치 알고리즘 완성
-        //미디어 컨트롤러는 호스트기기에만 뜨도록 설정 (완료)
+//미디어 컨트롤러는 호스트기기에만 뜨도록 설정
     }
 }
+//
