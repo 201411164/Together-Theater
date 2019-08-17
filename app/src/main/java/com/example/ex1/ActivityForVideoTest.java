f2f328be634f1a08ecbbf06387fc4ecdce6b184e
g5yoon <g5yoon1214@naver.com>
2019-08-17 13:26:13 +0900

    영상 배치를 위한 알고리즘 설계
    테스트용 변수값 지정
    
    가상 디바이스 에뮬레이터 실행이 되지 않아 추후에 다시 테스트 할 예정
    
    이해하기 편하게 주석 자세하게 달았음

--- app/src/main/java/com/example/ex1/ActivityForVideoTest.java
+++ app/src/main/java/com/example/ex1/ActivityForVideoTest.java
@@ -1,5 +1,6 @@
 package com.example.ex1;
 
+import android.media.MediaPlayer;
 import android.view.ViewGroup;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,11 +17,14 @@ public class ActivityForVideoTest extends AppCompatActivity {
     ConstraintSet mConstraintSet = new ConstraintSet();
     ConstraintLayout mConstraintLayout;
 
+
+
     int user = 1;//사용자 식별번호, 호스트 기기에만 미디어컨트롤러가 나오도록 하기 위함(user 변수의 값이 1인 경우에만 나오게 함)
     int aW,bW,cW,aH,bH,cH = 0;//화면 분할을 위한 각 디바이스 가로세로 길이
     int sH=0;//기기 a b c 중 가장 작은 높이값
     int W,H=0;//결정된 레이아웃의 길이
     int aX,bX,cX = 0;//좌표 이동을 위한 각 기기의 X값
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,7 +36,7 @@ public class ActivityForVideoTest extends AppCompatActivity {
         VideoView vv =(VideoView)findViewById(R.id.videoView1);
         Uri video = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.test2);
         vv.setVideoURI(video);
-
+        vv.start();
 
 
         //비디오뷰의 높이 설정을 위한 디바이스들 중 높이 최소값 구해 sH에 저장, 함수화 필요
@@ -74,5 +78,20 @@ public class ActivityForVideoTest extends AppCompatActivity {
 //화면 배치 알고리즘 완성
 //미디어 컨트롤러는 호스트기기에만 뜨도록 설정
     }
+
+
+    //영상 재생,정지 동기화
+    //일시정지
+    
+
+
+
+
 }
-//
+//일시정지
+//호스트 기기에서 일시정지를 누른 경우 일시정지 된 시간을 stopTime 변수에 저장 후 stopSignal과 stopTime을 클라이언트 기기로 전송
+//stopSignal을 받으면 stopTime 에 저장된 시간으로 영상 이동 후 일시정지
+
+//다시 재생
+//호스트 기기에서 재생을 누른 경우 현재시간을 구한 후 그로부터 +3sec 된 시간을 playTime 변수에 저장, playSignal과 함께 클라이언트 기기로 전송
+//playSignal을 받으면 playTime 변수에 저장된 시간에 재생 시작작
