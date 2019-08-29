31e6da2db074c4064f720e66b16b7e7503cace52
g5yoon <g5yoon1214@naver.com>
2019-08-29 13:27:41 +0900

    이해하기 편하게 주석 자세하게 달았음

--- app/src/main/java/com/example/ex1/ActivityForVideoTest.java
+++ app/src/main/java/com/example/ex1/ActivityForVideoTest.java
@@ -1,11 +1,17 @@
 package com.example.ex1;
 
+
+import android.content.Context;
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Handler;
+import android.view.View;
 import android.view.ViewGroup;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.VideoView;
 import android.widget.MediaController;
+import android.widget.Button;
 import android.net.Uri;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
@@ -14,29 +20,48 @@ import androidx.constraintlayout.widget.ConstraintSet;
 
 public class ActivityForVideoTest extends AppCompatActivity {
     //컨스트레인트 레이아웃 정의
-    ConstraintSet mConstraintSet = new ConstraintSet();
-    ConstraintLayout mConstraintLayout;
 
 
 
     int user = 1;//사용자 식별번호, 호스트 기기에만 미디어컨트롤러가 나오도록 하기 위함(user 변수의 값이 1인 경우에만 나오게 함)
     int aW,bW,cW,aH,bH,cH = 0;//화면 분할을 위한 각 디바이스 가로세로 길이
-    int sH=0;//기기 a b c 중 가장 작은 높이값
-    int W,H=0;//결정된 레이아웃의 길이
+    int sH = 0;//기기 a b c 중 가장 작은 높이값
+    int W,H = 0;//결정된 레이아웃의 길이
     int aX,bX,cX = 0;//좌표 이동을 위한 각 기기의 X값
 
+    int stopTime = 0;
+    VideoView vv;
+    Button btnStart,btnPause;
 
+    private MediaPlayer.OnVideoSizeChangedListener onVIdeoSizeChangedListener = new MediaPlayer.OnVideoSizeChangedListener(){
+        public void onVideoSizeChanged(MediaPlayer mp,int width, int height){
+            LayoutParams lp = new
+        }
+    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //컨스트레인트 레이아웃 내부에 비디오뷰 생성 비디오뷰 사이즈는 레이아웃과 동일, 이후 비디오뷰 위치 조정은 setX 매소드를 이용
         setContentView(R.layout.activity_for_video_test);
-        mConstraintLayout = (ConstraintLayout) findViewById(R.id.cL1);
-        mConstraintSet.clone(mConstraintLayout);
-        VideoView vv =(VideoView)findViewById(R.id.videoView1);
+        //시작,일시정지 버튼
+        btnStart = (Button)findViewById(R.id.btnStart);
+        btnPause = (Button)findViewById(R.id.btnPause);
+        //비디오뷰 생성
+        final VideoView vv = new VideoView(this);
+
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT,ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.height=1920;
+        layoutParams.width=1080;
+        ConstraintLayout constraintLayout = findViewById(R.id.cL1);
+        constraintLayout.setLayoutParams(layoutParams);
         Uri video = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.test2);
         vv.setVideoURI(video);
+        vv.setX(0);
+        if (constraintLayout != null){
+            constraintLayout.addView(vv);
+        }
         vv.start();
+//레이아웃 사이즈 조정(layoutparams 매소드 이용)
 
 
         //비디오뷰의 높이 설정을 위한 디바이스들 중 높이 최소값 구해 sH에 저장, 함수화 필요
@@ -56,23 +81,21 @@ public class ActivityForVideoTest extends AppCompatActivity {
         H=sH;
 
 
-        //레이아웃 사이즈 조정(layoutparams 매소드 이용)
-        setContentView(R.layout.activity_for_video_test);
-        ConstraintLayout.LayoutParams mLayoutParams = (ConstraintLayout.LayoutParams)vv.getLayoutParams();
-        mLayoutParams.height=1920;
-        mLayoutParams.width=1080;
-        mConstraintLayout.setLayoutParams(mLayoutParams);
 
 
-        //미디어 컨트롤러 생성
-        MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView(vv);
-        mediaController.setPadding(0,0,0,0);
-        vv.setMediaController(mediaController);
+
+        //user 변수의 값이 1일 경우(=호스트 기기일 경우) 미디어 컨트롤러 생성
+        if(user==1){
+            MediaController mediaController = new MediaController(this);
+            mediaController.setAnchorView(vv);
+            mediaController.setPadding(0,0,0,0);
+            vv.setMediaController(mediaController);
+        }
+        //자동재생시작
         vv.start();
 
 //현재과제
-//별도의 변수(int user) 추가하여 각 기기마다 독립적 view 제공
+//별도의 변수(int user) 추가하여 각 기기마다 독립적 액티비티 제공
 //기기 배치를 위한 가이드라인 제공
 //영상 일시정지 시 정지한 시간 다른 기기로 전송, 그 시간에 맞추어 정지
 //다시 재생 시 정해진 시간(위성시계 기준)에 동시 재생 시작
@@ -81,10 +104,33 @@ public class ActivityForVideoTest extends AppCompatActivity {
     }
 
 
+    public void StartButton(View v){
+        playVideo();
+    }
+    public void PauseButton(View v){
+        pauseVideo();
+    }
+
     //영상 재생,정지 동기화
     //일시정지
-
-
+    public void pauseVideo(){
+        vv.pause();
+        stopTime = vv.getCurrentPosition();
+        vv.seekTo(stopTime);
+    }
+    //재생
+    //Handler 이용해 현재 시간으로부터 2000밀리세컨드 후에 재생 Method 호출(vv.start())
+    public void playVideo(){
+        vv.seekTo(stopTime);
+        long cT = System.currentTimeMillis();
+        long playWhen = cT + 2000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                vv.start();
+            }
+        }, playWhen);
+    }
 
 
 
@@ -95,4 +141,4 @@ public class ActivityForVideoTest extends AppCompatActivity {
 
 //다시 재생
 //호스트 기기에서 재생을 누른 경우 현재시간을 구한 후 그로부터 +3sec 된 시간을 playTime 변수에 저장, playSignal과 함께 클라이언트 기기로 전송
-//playSignal을 받으면 playTime 변수에 저장된 시간에 재생 시작작
+//playSignal을 받으면 playTime 변수에 저장된 시간에 재생 시작
