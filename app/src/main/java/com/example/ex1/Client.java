9e5a501f6a39f7d1da168941a2135ce0bc245955
g5yoon <g5yoon1214@naver.com>
2019-09-02 23:47:49 +0900

    2019-09-02 액티비티 일시정지, 복귀, 종료 관련된 매서드 추가 다른 액티비티로부터 뒤로가기버튼, 종료 관련 인텐트 전달받을 시 실행되어야 하는 메소드 추가

--- app/src/main/java/com/example/ex1/Client.java
+++ app/src/main/java/com/example/ex1/Client.java
@@ -12,12 +12,14 @@ import android.net.Uri;
 public class Client extends AppCompatActivity {
 
     int user = 2;//사용자 식별번호, 호스트 기기에만 미디어컨트롤러가 나오도록 하기 위함(user 변수의 값이 1인 경우에만 나오게 함)
-    int H = 1500;//결정된 레이아웃의 길이
-    int W = 3000;
+    int H = 0;//결정된 레이아웃의 길이
+    int W = 0;
     int aW, bW, cW, aH, bH, cH = 0;
     int aX, bX, cX = 0;//좌표 이동을 위한 각 기기의 X값
     int aY, bY, cY = 0;//좌표 이동을 위한 각 기기의 Y값
-    int stopTime = 0;
+    public int back = 0;
+    public int stopTime = 0;
+    String filename;
     VideoView vv;
 
 
@@ -26,7 +28,8 @@ public class Client extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
         vv = findViewById(R.id.videoView1);
-        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.test2);
+        //filename = this.getExternalFilesDir(null) + "/TogetherTheater";
+        Uri video = Uri.parse("android.resource://" + getPackageName() + "/"+ R.raw.test2);
         vv.setVideoURI(video);
         ViewGroup.LayoutParams params = vv.getLayoutParams();
         params.height = H;
@@ -42,4 +45,44 @@ public class Client extends AppCompatActivity {
         stopTime = vv.getCurrentPosition();
         //vv.seekTo(stopTime);
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        VideoView vv = findViewById(R.id.videoView1);
+        vv.resume();
+        if (stopTime > 0) {
+            vv.seekTo(stopTime);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        VideoView vv = findViewById(R.id.videoView1);
+        if (vv.canPause()) {
+            stopTime = vv.getCurrentPosition();
+            pauseVideo();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        VideoView vv = findViewById(R.id.videoView1);
+        vv.stopPlayback();
+        super.onDestroy();
+    }
+    //뒤로가기 버튼 눌러서 액티비티 종료한 경우(다른 기기로 종료 신호 인텐트 전달 기능 추가 필요)
+    @Override
+    public void onBackPressed() {
+        if ( back == 1){
+            super.onBackPressed();
+            back = 0;
+        }
+    }
+    //Host 액티비티로부터 액티비티 종료 인텐트를 전달받을 경우에 실행되어야 함
+    public void quitByHost(){
+        finish();
+    }
 }
+//온터치 , 온트랙볼 이벤트 ( 재생,일시정지,영상위치이동 ) - 미디어컨트롤러로 대체
+//액티비티 종료할 시 모든 기기에서 액티비티 종료( 이전 액티비티로 이동)
--- app/src/main/java/com/example/ex1/Host.java
+++ app/src/main/java/com/example/ex1/Host.java
@@ -3,10 +3,10 @@ package com.example.ex1;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.VideoView;
-//import android.widget.MediaController;
 import android.widget.Button;
 import android.net.Uri;
 
@@ -20,7 +20,8 @@ public class Host extends AppCompatActivity {
     int W = 3000;
     int aX, bX, cX = 0;//좌표 이동을 위한 각 기기의 X값
     int aY, bY, cY = 0;
-    int stopTime = 0;
+    public int stopTime = 0;
+    public int back = 0;
     VideoView vv;
     Button btnStart, btnPause;
 
@@ -35,7 +36,7 @@ public class Host extends AppCompatActivity {
         vv = findViewById(R.id.videoView1);
         Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.test2);
         vv.setVideoURI(video);
-
+        mediaController();
         //비디오뷰의 높이 설정을 위한 디바이스들 중 높이 최소값 구해 sH에 저장
         if (aH < bH) {
             if (aH < cH) {
@@ -60,6 +61,9 @@ public class Host extends AppCompatActivity {
         aX = 0;
         bX = -aW;
         cX = -aW - bW;
+        aY = aH - H;
+        bY = bH - H;
+        cY = cH - H;
 
         //비디오뷰 사이즈 조절
         ViewGroup.LayoutParams params = vv.getLayoutParams();
@@ -69,15 +73,17 @@ public class Host extends AppCompatActivity {
         vv.setX(aX);
         vv.setY(aY);
     }
+
     //user 변수의 값이 1일 경우(=호스트 기기일 경우) 미디어 컨트롤러 생성
-    //public void mediaController () {
-    //    if (user == 1) {
-    //        MediaController mediaController = new MediaController(this);
-    //        mediaController.setAnchorView(vv);
-    //        mediaController.setPadding(0, 0, 0, 0);
-    //        vv.setMediaController(mediaController);
-    //    }
-    //}
+    public void mediaController() {
+        if (user == 1) {
+            MediaController mediaController = new MediaController(this);
+            mediaController.setAnchorView(vv);
+            mediaController.setPadding(0, 0, 0, 0);
+            vv.setMediaController(mediaController);
+
+        }
+    }
 
 
     public void StartButton(View v) {
@@ -89,7 +95,7 @@ public class Host extends AppCompatActivity {
     }
 
     //영상 재생,정지 동기화
-    //일시정지
+    //일시정지 - 일시정지 신호를 다른 기기로 전달하는 기능 추가 필요
     public void pauseVideo() {
         vv.getCurrentPosition();
         vv.pause();
@@ -97,25 +103,53 @@ public class Host extends AppCompatActivity {
         //vv.seekTo(stopTime);
     }
 
-    //재생
+    //재생 - 재생 신호를 다른 기기로 전달하는 기능 추가 필요
     //Handler 이용해 현재 시간으로부터 2000밀리세컨드 후에 재생 Method 호출(vv.start())
     public void playVideo() {
         //vv.seekTo(stopTime);
         vv.start();
     }
-}
-//일시정지
-//호스트 기기에서 일시정지를 누른 경우 일시정지 된 시간을 stopTime 변수에 저장 후 stopSignal과 stopTime을 클라이언트 기기로 전송
-//stopSignal을 받으면 stopTime 에 저장된 시간으로 영상 이동 후 일시정지
 
-//다시 재생
-//호스트 기기에서 재생을 누른 경우 현재시간을 구한 후 그로부터 +2sec 된 시간을 playTime 변수에 저장, playSignal과 함께 클라이언트 기기로 전송
-//playSignal을 받으면 playTime 변수에 저장된 시간에 재생 시작
-// 현재과제
-////별도의 변수(int user) 추가하여 각 기기마다 독립적 액티비티 제공
-////기기 배치를 위한 가이드라인 제공
-////영상 일시정지 시 정지한 시간 다른 기기로 전송, 그 시간에 맞추어 정지
-////다시 재생 시 정해진 시간(위성시계 기준)에 동시 재생 시작
-////화면 배치 알고리즘 완성(레이아웃과 뷰 연결)
-////미디어 컨트롤러는 호스트기기에만 뜨도록 설정
-//
+    @Override
+    public void onResume() {
+        super.onResume();
+        VideoView vv = findViewById(R.id.videoView1);
+        vv.resume();
+        if (stopTime > 0) {
+            vv.seekTo(stopTime);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        VideoView vv = findViewById(R.id.videoView1);
+        if (vv.canPause()) {
+            stopTime = vv.getCurrentPosition();
+            pauseVideo();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        VideoView vv = findViewById(R.id.videoView1);
+        vv.stopPlayback();
+        super.onDestroy();
+    }
+
+    //뒤로가기 버튼 눌러서 액티비티 종료한 경우(다른 기기로 종료 신호 인텐트 전달 기능 추가 필요)
+    @Override
+    public void onBackPressed() {
+        if (back == 1) {
+            super.onBackPressed();
+            back = 0;
+        }
+    }
+
+    //Client 액티비티로부터 액티비티 종료 인텐트를 전달받았을 때 실행되어야 함
+    public void quitByClient() {
+        finish();
+    }
+}
+//pause가 걸리는 경우 - 전화, 다른 앱의 메세지, 팝업 등의 불가피한 pause ----나머지 기기들은 영상 일시정지
+//                   - 뒤로가기 버튼으로 임의로 액티비티 종료 --------------나머지 기기들은 액티비티 종료
